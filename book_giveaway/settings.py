ae712272acc216f2cc81170c2acb155a424ce304
sandro988 <nadiradzesandro98@gmail.com>
2023-09-26 13:51:01 +0400

    Dropped the default SQLite database from the project and started using PostgreSQL.

--- book_giveaway/settings.py
+++ book_giveaway/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "book_giveaway.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("NAME"),
+        "USER": env("USER"),
+        "PASSWORD": env("PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
